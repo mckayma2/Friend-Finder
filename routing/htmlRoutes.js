100b12eb1ed4b53a2bd1874fb4c92aed75ba0dd2
Mackay <mario.mackay@marineharvest.com>
2018-02-07 15:10:02 -0500

    added photo and fixed survey comparison somewhat

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,4 +1,5 @@
 var myArray = [];
+var resultArray =[];
 var totalScore;
 var path = require('path');
 var apiRoutes = require('./apiRoutes.js');
@@ -19,6 +20,7 @@ survey: function(app){
 
 app.post('/survey', function (req, res) {
 //console.log(req.body);
+req.body.name = req.body.name.toUpperCase();
 //res.sendFile(path.join(__dirname + './../html/survey.html'));
 if(req.body.name !== "" && req.body.PhotoURL !== "" && req.body.Scores1 !== "" && req.body.Scores1 <=5 && req.body.Scores2 !== ""  && req.body.Scores2 <=5 && req.body.Scores3 !== ""  && req.body.Scores3 <=5 && req.body.Scores4 !== ""  && req.body.Scores4 <=5 && req.body.Scores5 !== ""  && req.body.Scores5 <=5){
 // && req.body.PhotoURL !== null && req.body.Scores1 !== null && req.body.Scores1 <=5 && req.body.Scores2 !== null  && req.body.Scores2 <=5 && req.body.Scores3 !== null  && req.body.Scores3 <=5 && req.body.Scores4 !== null  && req.body.Scores4 <=5 && req.body.Scores5 !== null  && req.body.Scores5 <=5  
@@ -28,18 +30,25 @@ myArray.push(mySurvey);
 routes.api(app, myArray);
 console.log(myArray);
 if (myArray.length > 1){
-for (var i =0; i <= myArray.length; i++){
+		function search(nameKey, myArray){
+   		for (var i=0; i < myArray.length; i++) {
+       	 if (myArray[i].score === nameKey) {
+            return myArray[i].name;
+        }
+    }
+}
+
 	
-	for (key in myArray[i]){
-   		 
-	res.send('Your Submission was accepted\n <ul><li>Name: '+ req.body.name +'</li> <li> PhotoURL: '+ req.body.PhotoURL + '</li> <li>Score 1: '+ req.body.Scores1 +' </li> <li> Score 2:  '+ req.body.Scores2 +' </li> <li> Score 3: '+ req.body.Scores3 +' </li> <li> Score 4: '+ req.body.Scores4 +' </li> <li>Score 5:  '+ req.body.Scores5 +'</li><li> TOTAL SCORE: '+ sum + '</li></ul> \n <form action="/survey" target="_blank" ><input type="submit" value="Return to Survey" /></form>');
 	
- 	console.log(myArray[i][key]);
- 	};
-};
+
+	var resultObject = search(sum, myArray);
+	resultObject =resultObject.toUpperCase();
+	res.send('Your Submission was accepted\n <ul><li>Name: '+ req.body.name +'</li> <li> PhotoURL: <img src='+ req.body.PhotoURL + ' alt='+ req.body.name +' image style="width:500px;height:150px;"></li> <li>Score 1: '+ req.body.Scores1 +' </li> <li> Score 2:  '+ req.body.Scores2 +' </li> <li> Score 3: '+ req.body.Scores3 +' </li> <li> Score 4: '+ req.body.Scores4 +' </li> <li>Score 5:  '+ req.body.Scores5 +'</li><li> TOTAL SCORE: '+ sum + '</li><li>You and '+ resultObject +' have the same score of '+ sum + '</li></ul> \n <form action="/survey" target="_blank" ><input type="submit" value="Return to Survey" /></form>');
+	// resultArray.push(resultObject);
+	// console.log(resultArray);
 }
 else{
-	res.send('Your Submission was accepted\n <ul><li>Name: '+ req.body.name +'</li> <li> PhotoURL: '+ req.body.PhotoURL + '</li> <li>Score 1: '+ req.body.Scores1 +' </li> <li> Score 2:  '+ req.body.Scores2 +' </li> <li> Score 3: '+ req.body.Scores3 +' </li> <li> Score 4: '+ req.body.Scores4 +' </li> <li>Score 5:  '+ req.body.Scores5 +'</li><li> TOTAL SCORE: '+ sum + '</li><li>You are the only user with a score of '+ sum + '</li></ul> \n <form action="/survey" target="_blank" ><input type="submit" value="Return to Survey" /></form>');
+	res.send('Your Submission was accepted\n <ul><li>Name: '+ req.body.name +'</li> <li> PhotoURL: <img src='+ req.body.PhotoURL + ' alt='+ req.body.name +' image style="width:500px;height:150px;"></li> <li>Score 1: '+ req.body.Scores1 +' </li> <li> Score 2:  '+ req.body.Scores2 +' </li> <li> Score 3: '+ req.body.Scores3 +' </li> <li> Score 4: '+ req.body.Scores4 +' </li> <li>Score 5:  '+ req.body.Scores5 +'</li><li> TOTAL SCORE: '+ sum + '</li><li>You are the only user with a score of '+ sum + '</li></ul> \n <form action="/survey" target="_blank" ><input type="submit" value="Return to Survey" /></form>');
 
 }
 
